2635f17aa7f1a6fa39202e9f1306f289ccecbab3
PRANAV K DILEEP <91619846+pranavkdileep@users.noreply.github.com>
2023-09-28 21:55:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const {GradioChatBot} = require('gradio-chatbot');
 
-const bot = new GradioChatBot();
+const bot = new GradioChatBot(
+  url = '1',
+  historySize = '1'
+);
+
 
 async function start(prompt) {
-  const message = await bot.chat(prompt, {
-  });
+  await bot.reset();
+  const message = await bot.chat(prompt);
   console.log('message', message);
+  await bot.reset();
   return message;
 }
 
+start("java code print hello pranav")
 
 module.exports = start;
